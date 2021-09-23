5a11a31e36cfb5ccbacc5df6c338554774258ee9
Emile Metral <metral4269@gmail.com>
2021-09-23 14:25:48 +0200

    e

--- TP2/justePrix.sh
+++ TP2/justePrix.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 nombre=$(( $RANDOM % 1000 + 1 ))
-echo $nombre
+findIt=0
+
+
+while [ $findIt -ne 1 ]
+do
+    read -s 'Entre un nombre ' SAISI
+
+    if [ $SAISI -gt $nombre ]; then
+        echo "Plus petit"
+    elif [ $SAISI -lt $nombre ]; then
+        echo "Plus grand"
+    else
+        echo "Bravo !"
+        findIt=1
+    fi
+done
+
+
